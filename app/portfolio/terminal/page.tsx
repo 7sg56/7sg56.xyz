eb059cdba0b0cf588ee11af4548e0b36e411b03f
Sourish Ghosh <sghosh.ile.7@gmail.com>
2026-01-12 23:55:35 +0530

    terminal name svg made responsive

--- app/portfolio/terminal/page.tsx
+++ app/portfolio/terminal/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
 import Terminal from "@/components/terminal/Terminal";
 import { generateUUID } from "@/lib/utils";
 
