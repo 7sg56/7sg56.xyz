603501d7b81f27401057f3594fcc08dd64c4ddb2
Sourish Ghosh <sghosh.ile.7@gmail.com>
2026-01-19 10:03:10 +0530

    removed algorithms pane

--- components/desktop/types.ts
+++ components/desktop/types.ts
@@ -1,4 +1,4 @@
-export type AppType = "about" | "projects" | "skills" | "contact" | "tetris" | "algorithms";
+export type AppType = "about" | "projects" | "skills" | "contact" | "tetris";
 export type WindowAppType = Exclude<AppType, "terminal">;
 
 export type DockApp = {
