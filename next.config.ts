4438e668942227ad98bc8d918d06912f14872625
Sourish Ghosh <sghosh.ile.7@gmail.com>
2026-05-04 23:00:36 +0530

    add sanity deps and config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     qualities: [75, 90],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+
+  // Required for Sanity Studio (uses styled-components)
+  compiler: {
+    styledComponents: true,
   },
 };
 
